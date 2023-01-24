b5cb6c974019d57887f5d675ef02e858aac10304
visu <visuvisu115@gmai.com>
2023-01-24 12:05:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//using switch case if you give a number which date on that day
+
+let Day = 2;
+switch (Day){
+  case 0 :
+   Day = 'sunday';
+   break;
+
+   case 1 :
+   Day = 'Monday';
+   break;
+
+   case 2 :
+   Day = 'Tuesday';
+   break;
+
+   case 3 :
+   Day = 'Wednesday';
+   break;
+
+   case 4 :
+   Day = 'Thursday';
+   break;
+
+   case 5 :
+   Day = 'Friday';
+   break;
+
+   case 6 :
+   Day = 'Saturday';
+   break;
+}
+console.log(Day);
